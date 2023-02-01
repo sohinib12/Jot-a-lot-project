d9cf5a510fc0cc458566d75c1d58d7190a5c1b55
sohinib12 <sohini.bonthala@gmail.com>
2023-02-01 15:11:24 -0800

    css styles updated for splash, login and signup pages

--- app/forms/signup_form.py
+++ app/forms/signup_form.py
@@ -24,5 +24,5 @@ class SignUpForm(FlaskForm):
     username = StringField(
         'username', validators=[DataRequired(), username_exists])
     email = StringField('email', validators=[
-                        DataRequired(), Email(message= "Please enter a valid email address.") user_exists])
+                        DataRequired(), user_exists])
     password = StringField('password', validators=[DataRequired()])
