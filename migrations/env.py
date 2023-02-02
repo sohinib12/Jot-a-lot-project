45f7300c4866c806c16990d750360c9261ed9a19
sohinib12 <sohini.bonthala@gmail.com>
2023-02-02 08:20:09 -0800

    render debugging

--- migrations/env.py
+++ migrations/env.py
@@ -1,15 +1,19 @@
+
 from __future__ import with_statement
-from sqlalchemy import engine_from_config, pool
+from flask import current_app
+
 import logging
 from logging.config import fileConfig
 
-from flask import current_app
+from sqlalchemy import engine_from_config
+from sqlalchemy import pool
 
 from alembic import context
 
 import os
 environment = os.getenv("FLASK_ENV")
 SCHEMA = os.environ.get("SCHEMA")
+
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -19,16 +23,6 @@ config = context.config
 # This line sets up loggers basically.
 fileConfig(config.config_file_name)
 logger = logging.getLogger('alembic.env')
-
-
-# def get_engine():
-#     try:
-#         # this works with Flask-SQLAlchemy<3 and Alchemical
-#         return current_app.extensions['migrate'].db.get_engine()
-#     except TypeError:
-#         # this works with Flask-SQLAlchemy>=3
-#         return current_app.extensions['migrate'].db.engine
-
 
 # add your model's MetaData object here
 # for 'autogenerate' support
@@ -45,27 +39,18 @@ target_metadata = current_app.extensions['migrate'].db.metadata
 # ... etc.
 
 
-# def get_metadata():
-#     if hasattr(target_db, 'metadatas'):
-#         return target_db.metadatas[None]
-#     return target_db.metadata
-
-
 def run_migrations_offline():
     """Run migrations in 'offline' mode.
-
     This configures the context with just a URL
     and not an Engine, though an Engine is acceptable
     here as well.  By skipping the Engine creation
     we don't even need a DBAPI to be available.
-
     Calls to context.execute() here emit the given string to the
     script output.
-
     """
     url = config.get_main_option("sqlalchemy.url")
     context.configure(
-        url=url, target_metadata=get_metadata(), literal_binds=True
+        url=url, target_metadata=target_metadata, literal_binds=True
     )
 
     with context.begin_transaction():
@@ -74,10 +59,8 @@ def run_migrations_offline():
 
 def run_migrations_online():
     """Run migrations in 'online' mode.
-
     In this scenario we need to create an Engine
     and associate a connection with the context.
-
     """
 
     # this callback is used to prevent an auto-migration from being generated
